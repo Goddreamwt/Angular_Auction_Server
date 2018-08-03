ba58e94707fa456323b8d8ca6a1b05be35c3eb4f
Goddreamwt <zm804890716@163.com>
2018-08-03 14:02:38 +0800

    完善商品搜索功能

--- server/build/auction_server.js
+++ server/build/auction_server.js
@@ -2,12 +2,24 @@
 var express = require("express");
 var ws_1 = require('ws');
 var app = express();
-
 app.get('/', function (req, res) {
     res.send("Hello Express");
 });
 app.get('/api/products', function (req, res) {
-    res.json(products);
+    var result = products;
+    var params = req.query;
+    console.log(params);
+    if (params.title) {
+        result = result.filter(function (p) { return p.title.indexOf(params.title) !== -1; });
+    }
+    if (params.price && params.price !=='null' && result.length > 0) {
+        result = result.filter(function (p) { return p.price <= parseInt(params.price); });
+    }
+    if (params.category &&params.category !== "-1"&& result.length > 0) {
+        result = result.filter(function (p) { return p.categories.indexOf(params.category) !== -1; });
+    }
+    console.log(result);
+    res.json(result);
 });
 app.get('/api/product/:id', function (req, res) {
     res.json(products.find(function (product) { return product.id == req.params.id; }));
@@ -57,19 +69,18 @@ var Comment = (function () {
     return Comment;
 }());
 exports.Comment = Comment;
-
 var products = [
     new Product(1, '第一个商品', 1.99, 3.5, "这是第一商品，asdxc奥术大师多撒", ["电子产品", "硬件设备", "其他"]),
     new Product(2, '第二个商品', 2.99, 2.5, "这是第二商品，奥术大师多驱蚊器二无", ["硬件设备", "其他"]),
     new Product(3, '第三个商品', 3.99, 1.5, "这是第三商品，请问驱蚊器翁群翁", ["电子产品", "硬件设备"]),
-    new Product(4, '第四个商品', 4.99, 2.0, "这是第四商品，切勿驱蚊器翁", ["电子产品", "其他"]),
-    new Product(5, '第五个商品', 5.99, 3.5, "这是第五商品，213123123", ["电子产品", "硬件设备", "其他"]),
-    new Product(6, '第六个商品', 6.99, 4.5, "这是第六商品，啊多少大所多多", ["电子产品", "硬件设备", "其他"])
+    new Product(4, '第四个商品', 4.99, 2.0, "这是第四商品，切勿驱蚊器翁", ["电子产品"]),
+    new Product(5, '第五个商品', 5.99, 3.5, "这是第五商品，213123123", ["电子产品", "硬件设备"]),
+    new Product(6, '第六个商品', 6.99, 4.5, "这是第六商品，啊多少大所", ["电子产品", "硬件设备", "其他"])
 ];
 var comments = [
     new Comment(1, 1, "2017-02-02 22:22:22", "张三", 3, "东西不错"),
     new Comment(2, 2, "2017-03-02 23:22:22", "李四", 4, "东西挺不错"),
-    new Comment(3, 1, "2017-04-02 24:22:22", "王五", 2, "东西不错"),
-    new Comment(4, 1, "2017-05-02 25:22:22", "赵六", 1, "东西还不错"),
-    new Comment(5, 1, "2017-06-02 26:22:22", "哈哈", 3, "东西不错"),
+    new Comment(3, 3, "2017-04-02 24:22:22", "王五", 2, "东西不错"),
+    new Comment(4, 4, "2017-05-02 25:22:22", "赵六", 1, "东西还不错"),
+    new Comment(5, 5, "2017-06-02 26:22:22", "哈哈", 3, "东西不错"),
 ];
--- server/server/auction_server.ts
+++ server/server/auction_server.ts
@@ -4,29 +4,24 @@ import {Server} from 'ws';
 const app = express();
 
 
-const products: Product[] = [
-    new Product(1, '第一个商品', 1.99, 3.5, "这是第一商品，asdxc奥术大师多撒", ["电子产品", "硬件设备", "其他"]),
-    new Product(2, '第二个商品', 2.99, 2.5, "这是第二商品，奥术大师多驱蚊器二无", ["硬件设备", "其他"]),
-    new Product(3, '第三个商品', 3.99, 1.5, "这是第三商品，请问驱蚊器翁群翁", ["电子产品", "硬件设备"]),
-    new Product(4, '第四个商品', 4.99, 2.0, "这是第四商品，切勿驱蚊器翁", ["电子产品", "其他"]),
-    new Product(5, '第五个商品', 5.99, 3.5, "这是第五商品，213123123", ["电子产品", "硬件设备", "其他"]),
-    new Product(6, '第六个商品', 6.99, 4.5, "这是第六商品，啊多少大所多多", ["电子产品", "硬件设备", "其他"])
-];
-
-const comments: Comment[] = [
-    new Comment(1, 1, "2017-02-02 22:22:22", "张三", 3, "东西不错"),
-    new Comment(2, 2, "2017-03-02 23:22:22", "李四", 4, "东西挺不错"),
-    new Comment(3, 1, "2017-04-02 24:22:22", "王五", 2, "东西不错"),
-    new Comment(4, 1, "2017-05-02 25:22:22", "赵六", 1, "东西还不错"),
-    new Comment(5, 1, "2017-06-02 26:22:22", "哈哈", 3, "东西不错"),
-];
-
-
 app.get('/', (req, res) => {
     res.send("Hello Express");
 });
 
 app.get('/api/products', (req, res) => {
+    let result = products
+    let params = req.query;
+    if (params.title) {
+        result =result.filter((p) => p.title.indexOf(params.title) !== -1);
+    }
+
+    if (params.price  && params.price !=='null'&&  result.length > 0) {
+        result =result.filter((p) => p.price <= parseInt(params.price));
+    }
+
+    if (params.category && params.category !== "-1" && result.length > 0) {
+        result =result.filter((p) => p.categories.indexOf(params.category) !== -1);
+    }
     res.json(products);
 });
 
@@ -58,7 +53,7 @@ setInterval(() => {
             client.send("这是定时推送");
         })
     }
-},2000);
+}, 2000);
 
 
 export class Product {
@@ -82,3 +77,21 @@ export class Comment {
 
     }
 }
+
+
+const products: Product[] = [
+    new Product(1, '第一个商品', 1.99, 3.5, "这是第一商品，asdxc奥术大师多撒", ["电子产品", "硬件设备", "其他"]),
+    new Product(2, '第二个商品', 2.99, 2.5, "这是第二商品，奥术大师多驱蚊器二无", ["硬件设备", "其他"]),
+    new Product(3, '第三个商品', 3.99, 1.5, "这是第三商品，请问驱蚊器翁群翁", ["电子产品", "硬件设备"]),
+    new Product(4, '第四个商品', 4.99, 2.0, "这是第四商品，切勿驱蚊器翁", ["电子产品", "其他"]),
+    new Product(5, '第五个商品', 5.99, 3.5, "这是第五商品，213123123", ["电子产品", "硬件设备", "其他"]),
+    new Product(6, '第六个商品', 6.99, 4.5, "这是第六商品，啊多少大所多多", ["电子产品", "硬件设备", "其他"])
+];
+
+const comments: Comment[] = [
+    new Comment(1, 1, "2017-02-02 22:22:22", "张三", 3, "东西不错"),
+    new Comment(2, 2, "2017-03-02 23:22:22", "李四", 4, "东西挺不错"),
+    new Comment(3, 3, "2017-04-02 24:22:22", "王五", 2, "东西不错"),
+    new Comment(4, 4, "2017-05-02 25:22:22", "赵六", 1, "东西还不错"),
+    new Comment(5, 5, "2017-06-02 26:22:22", "哈哈", 3, "东西不错"),
+];
